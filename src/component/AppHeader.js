0e785f22ddd781b19ea2d2f8c174ecde1a96a90e
Igor Zeljkovic <igor.zeljkovic@vivifyideas.com>
2019-07-17 16:31:17 +0200

    Movie list styles

---
+++ src/component/AppHeader.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { logout } from '../store/actions/AuthActions';
+
+function AppHeader({ user, logout }) {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <Link className="navbar-brand" to="/home">Pocket IMDB</Link>
+      <div className="collapse navbar-collapse justify-content-end">
+        
+          {
+            user
+            ? (
+              <ul className="navbar-nav ">
+                <li className="nav-item">
+                  <Link
+                    className="nav-link"
+                    to="#"
+                    onClick={ logout }
+                  >
+                    Logout
+                  </Link>
+                </li>
+              </ul>
+            ) : (
+              <ul className="navbar-nav ">
+                <li className="nav-item">
+                  <Link className="nav-link" to="/login">Login</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/register">Register</Link>
+                </li>
+              </ul>
+            )
+          }
+      </div>
+    </nav>
+  )
+}
+
+const mapDispatchToProps = {
+  logout
+}
+
+export default connect(null, mapDispatchToProps)(AppHeader);
